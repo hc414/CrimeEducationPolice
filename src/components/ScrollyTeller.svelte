c70ad79a5fd467cbe8814d5b5de1c348cc2f6280
SamuelKao <samueloak8@gmail.com>
2024-05-24 15:38:07 -0700

    Update ScrollyTeller.svelte

--- src/components/ScrollyTeller.svelte
+++ src/components/ScrollyTeller.svelte
@@ -1,3 +1,9 @@
+<<<<<<< Updated upstream
+=======
+<script>
+  import Scroller from "@sveltejs/svelte-scroller";
+  import MyChart from "./MyChart.svelte";
+>>>>>>> Stashed changes
 
 <script>
   import { onMount } from "svelte";
@@ -33,6 +39,7 @@
     };
   });
 </script>
+<<<<<<< Updated upstream
 
 <div class="foreground">
   {#each Array(6) as _, index}
@@ -43,6 +50,18 @@
       <section>This is section {index + 1}</section>
     {/if}
     <!-- <section>This is section {index + 1}</section> -->
+=======
+<div>
+  <MyChart />
+</div>
+<style>
+  .background {
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    outline: green solid 3px;
+  }
+>>>>>>> Stashed changes
 
     <!-- add visulation page to each section below -->
     <!-- {#if index == 0}
@@ -62,3 +81,38 @@
   }
 </style>
 
+<<<<<<< Updated upstream
+=======
+<Scroller
+  top={0.0}
+  bottom={1}
+  threshold={0.5}
+  bind:count
+  bind:index
+  bind:offset
+  bind:progress
+>
+  <div class="background" slot="background">
+    <div class="progress-bars">
+      <p>current section: <strong>{index + 1}/{count}</strong></p>
+      <progress value={count ? (index + 1) / count : 0} />
+
+      <p>offset in current section</p>
+      <progress value={offset || 0} />
+
+      <p>total progress</p>
+      <progress value={progress || 0} />
+    </div>
+  </div>
+
+  <div class="foreground" slot="foreground">
+    <section>This is the first section.</section>
+    <section>This is the second section.</section>
+    <section>This is the third section.</section>
+    <section>This is the 4 section.</section>
+    <section>This is the 5 section.</section>
+    <section>This is the 6 section.</section>
+  </div>
+
+</Scroller>
+>>>>>>> Stashed changes

d43d489b585885595a1db7ff9a2229e5ec817a88
Howard Chen <howche123321@mail.hamastar.com.tw>
2024-05-24 19:13:13 -0700

    added select county and select year + formatting

--- src/components/CalMap.svelte
+++ src/components/CalMap.svelte
@@ -1,31 +1,22 @@
-
-<!-- <script>
-    import { onMount } from "svelte";
-    import cal from "./caCountiesTopoSimple.json";
-    import * as d3 from "d3";
-    import { feature, mesh } from "topojson-client";
-
-  
--->
-
-
 <script>
     import { onMount } from "svelte";
     import { feature, mesh } from "topojson-client";
     import { geoMercator, geoPath } from 'd3-geo';
-    import cal from "./caCountiesTopoSimple.json";
     import * as d3 from "d3";
+    import cal from "./caCountiesTopoSimple.json";
 
     let width = 650;
     let height = 600;
+    let selectedCounty = ""; // Variable to store the selected county
 
     let projection = geoMercator()
-        .scale(1000 * 2)
-        .center([-120, 36])
+        .scale(1000 * 2.4)
+        .center([-119, 37.5])
         .translate([width / 2, height / 2]);
 
     let path = geoPath()
         .projection(projection);
+
 
     onMount(() => {
         const svg = d3.select("#map").append("svg")
@@ -42,6 +33,7 @@
             .style("opacity", 0);
 
         svg.selectAll(".subunit")
+
             .data(feature(cal, cal.objects.subunits).features)
             .enter().append("path")
             .attr("class", d => "subunit " + d.properties.name)
@@ -58,15 +50,66 @@
                 div.transition()
                     .duration(500)
                     .style("opacity", 0);
+            })
+            .on("click", function(event, d) {
+                selectedCounty = d.properties.fullName;
+                highlightCounty();
             });
 
         svg.append("path")
             .datum(mesh(cal, cal.objects.subunits, (a, b) => a === b))
             .attr("class", "exterior-boundary")
             .attr("d", path);
+            
+        function highlightCounty() {
+            svg.selectAll(".subunit").style("fill", d => {
+                console.log(d.properties.fullName === selectedCounty);
+                return d.properties.fullName === selectedCounty ? "orange" : "steelblue";
+            });
+        }
+
+        // Event listener for dropdown change
+        document.getElementById("countyDropdown").addEventListener("change", function(event) {
+            selectedCounty = event.target.value;
+            highlightCounty();
+        });
+
+        function updateSelectedYear() {
+            const slider = document.getElementById('yearSlider');
+            const selectedYear = document.getElementById('selectedYear');
+
+            selectedYear.textContent = slider.value;
+
+            slider.addEventListener('input', function() {
+                selectedYear.textContent = this.value;
+                console.log(selectedYear.textContent);
+            });
+        }
+
+        // Call updateSelectedYear function directly in onMount
+        updateSelectedYear();
+        
+        const countiesData = feature(cal, cal.objects.subunits).features.map(d => d.properties.fullName);
+        const dropdown = document.getElementById("countyDropdown");
+        countiesData.sort();
+        countiesData.forEach(county => {
+            const option = document.createElement("option");
+            option.value = county;
+            option.textContent = county;
+            dropdown.appendChild(option);
+        });
     });
 </script>
-<h1>California TopoJSON with D3</h1>
-<div id="map"></div>
+
+<!-- <h1>California TopoJSON with D3</h1> -->
+
+<select id="countyDropdown">
+    <option value="">Select a County</option>
+</select>
+<div id="map">
+    
+</div>
+<input type="range" min="2013" max="2022" value="2013" class="slider" id="yearSlider">
+<p>Selected Year: <span id="selectedYear">2013</span></p>
 
 
--- src/components/ScrollyTeller.svelte
+++ src/components/ScrollyTeller.svelte
@@ -3,7 +3,6 @@
   import { onMount } from "svelte";
   import Police from "./Police.svelte";
   import CalMap from "./CalMap.svelte"
-  // import { currentPage } from "./store.js"; // import the store
 
   let sections, currentPage;
 
@@ -34,27 +33,121 @@
   });
 </script>
 
+<!-- <div class="foreground">
+  {#each Array(6) as _, index}
+    
+    {#if index == 0}
+      <section class="section-map">
+        <h1 class="header">This is section {index + 1} </h1>
+
+        <div class="map-container">
+          <CalMap />
+        </div>
+      </section>
+    {:else}
+      <section>
+        <h1 class="header">This is section {index + 1} </h1>
+        This is section {index + 1}</section>
+    {/if}
+  {/each}
+</div> -->
 <div class="foreground">
   {#each Array(6) as _, index}
-    <h1>This is section {index + 1} </h1>
     {#if index == 0}
-      <section><CalMap /></section>
+      <div class="big-section">
+        <h1 class="header">This is section {index + 1}</h1>
+        <section class="section-map">
+          <div class="left">
+            <h1 class="header">This is section {index + 1}</h1>
+          </div>
+          <div class="right">
+            <CalMap />
+          </div>
+        </section>
+      </div>
+      
     {:else}
-      <section>This is section {index + 1}</section>
+      <div class="big-section">
+        <h1 class="header">This is section {index + 1}</h1>
+        <section class="section-map">
+          <div class="left">
+            <h1 class="header">This is section {index + 1}</h1>
+          </div>
+          <div class="right">
+            <h1 class="header">This is section {index + 1}</h1>
+          </div>
+        </section>
+      </div>
     {/if}
-    <!-- <section>This is section {index + 1}</section> -->
-
-    <!-- add visulation page to each section below -->
-    <!-- {#if index == 0}
-      <CalMap />
-    {/if} -->
   {/each}
 </div>
 
-<style global>
+<!-- <style global>
   @import 'public/global.css';
   section {
     height: 100vh;
+    width: 100%;
+    display: grid;
+    justify-content: center;
+    align-items: center;
+  }
+  .section-map {
+    height: 100vh;
+    width: 100%;
+    display: grid;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .map-container {
+    height: 100vh;
+    width: 100%;
+    display: grid;
+    justify-content: right;
+    align-items: right;
+  }
+
+</style> -->
+<style global>
+  @import 'public/global.css';
+
+  section {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .section-map {
+    display: grid;
+    grid-template-columns: 1fr 1fr; /* Split the section into two equal halves */
+    height: 100vh;
+    width: 100%;
+  }
+
+  .left, .right {
+    display: grid;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .left {
+    justify-content: center;
+    align-items: center;
+  }
+
+  .right {
+    justify-content: center;
+    align-items: center;
+  }
+
+  .header {
+    text-align: center;
+  }
+
+  .map-container {
+    height: 80%;
     width: 100%;
     display: flex;
     justify-content: center;
